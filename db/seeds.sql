b16692f82dd2f79e7c658b97d9d9eeb684c793ae
kamalwc <carmichaelkw@gmail.com>
2020-06-06 01:54:27 -0400

    create database

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name, eaten) VALUES ('Vegey',false);
+
+INSERT INTO burgers (burger_name, eaten) VALUES ('Triple stack',true);
+
+INSERT INTO burgers (burger_name, eaten) VALUES ('Eggplant',false);
