79e3dfc1c804168d0f5ffbd2a8e3dfa1005ab3e2
kamalwc <carmichaelkw@gmail.com>
2020-06-07 15:52:34 -0400

    import orm into burger.js

---
+++ models/burger.js
@@ -0,0 +1,8 @@
+const orm = require('../config/orm.js')
+
+// orm.selectAll();
+// orm.updateOne(param);
+// orm.insertOne(param);
+
+//WHY DO WE NEED TO EXPORT AT THE END OF THE FILE 
+// module.exports;
