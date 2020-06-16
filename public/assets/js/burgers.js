2670d102c5706609d8a0ff79fa10eaf112df7382
kamalwc <carmichaelkw@gmail.com>
2020-06-16 00:07:21 -0400

    create front end for app

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,3 +1,64 @@
 
-// ( devouring , deletng , and throwing up refreshes the page when button clicked)
-location.reload;
+// location.reload;
+
+// $.ajax("/api/freshburgers",{ type: "get"}).then(
+//     function(data){
+//         console.log(data);
+//         for(let burgers of data)
+//     }
+// );
+
+$.ajax("/api/freshburgers",{ type: "get"}).then(
+    function(data){
+        // console.log(data[0].burger_name);
+        console.log(data.burgers);
+        data.burgers.forEach(i => {$('#eaten').append(
+            `<li>
+                <p>${i.burger_name}</p>
+                <button class='eat'>Eat me</button>
+            </li>`
+        )})
+    }
+);
+
+$.ajax("/api/eatenburgers",{ type: "get"}).then(
+    function(data){
+        // console.log(data[0].burger_name);
+        console.log(data.burgers);
+        data.burgers.forEach(i => {$('#not_eaten').append(
+            `<li>
+                <div class='burgerName'>
+                    <p>${i.burger_name}</p>
+                    <button class='digest'>Digest</button>
+                </div>
+            </li>`
+        )})
+    }
+);
+
+//put
+$('.eat').on('click',()=>{
+    $.ajax('/api/bugers',{type: 'PUT'}).then(
+        function(data){
+
+        }
+    )
+})
+
+//post 
+$('.eat').on('submit',()=>{
+    $.ajax('/api/bugers',{type: 'POST'}).then(
+        function(data){
+            
+        }
+    )
+})
+
+//delete
+$('.digest').on('click',()=>{
+    $.ajax('/api/bugers',{type: 'DELETE'}).then(
+        function(data){
+            
+        }
+    )
+})
--- server.js
+++ server.js
@@ -1,4 +1,26 @@
-const connection = require('./config/connection.js')
+const connection = require('./config/connection.js');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+require("./controllers/burgers_controller.js")(app);
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
 
 
 
