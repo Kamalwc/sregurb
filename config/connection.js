711a8aa9bcb9f2d9a7145b3cd14679d3d247d592
kamalwc <carmichaelkw@gmail.com>
2020-06-23 00:42:39 -0400

    JAWSDB edits

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "qf5dic2wzyjf1x5x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+    host: "localhost",
     port: 3306,
-    user: "iozelchyjlv6ni4p",
-    password: "jlj7861b94bmnyva",
-    database: "kvrmr59s63srfgl3"
+    user: "root",
+    password: "password",
+    database: "burgers_db"
   });
 }
   
