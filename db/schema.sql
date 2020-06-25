24522288c3d8ad77e386e3ef01d83612294f793c
kamalwc <carmichaelkw@gmail.com>
2020-06-25 18:17:11 -0400

    herou fix

--- db/schema.sql
+++ db/schema.sql
@@ -5,9 +5,12 @@ USE burgers_db;
 
 -- CREATE TABLE burgers
 -- (
--- id int NOT NULL AUTO_INCREMENT,
--- burger_name varchar(255) NOT NULL,
--- eaten boolean,
--- PRIMARY KEY (id)
--- )
+--     id int NOT NULL
+--     AUTO_INCREMENT,
+--  burger_name varchar
+--     (255) NOT NULL,
+--  eaten boolean,
+--  PRIMARY KEY
+--     (id)
+--  )
 
